92ef773e98d73282ffbd887c7abacf9e383eaa6c
Noah F <Franck.noahk@gmail.com>
2019-12-05 23:03:13 -0600

    Added the firebase initalization to each of the webpages. Support for analytics, performance, firestore, authentication.

---
+++ public/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAYktxW1_uDmjLzSAoecOMAlTKtI5sGpmY",
+    authDomain: "post-of-the-coast-dev.firebaseapp.com",
+    databaseURL: "https://post-of-the-coast-dev.firebaseio.com",
+    projectId: "post-of-the-coast-dev",
+    storageBucket: "post-of-the-coast-dev.appspot.com",
+    messagingSenderId: "921534180906",
+    appId: "1:921534180906:web:740a8c0d02ad8bc7381d53"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
