8170873f682335f81d95b2dae000fdbe73b713b3
Noah F <Franck.noahk@gmail.com>
2019-12-09 02:22:47 -0600

    Added function to print to consolie if loged in. Not fully sure everything is working

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,24 +2,60 @@
 
 
   // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAYktxW1_uDmjLzSAoecOMAlTKtI5sGpmY",
-    authDomain: "post-of-the-coast-dev.firebaseapp.com",
-    databaseURL: "https://post-of-the-coast-dev.firebaseio.com",
-    projectId: "post-of-the-coast-dev",
-    storageBucket: "post-of-the-coast-dev.appspot.com",
-    messagingSenderId: "921534180906",
-    appId: "1:921534180906:web:740a8c0d02ad8bc7381d53"
-  };
-  // Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyAYktxW1_uDmjLzSAoecOMAlTKtI5sGpmY",
+  authDomain: "post-of-the-coast-dev.firebaseapp.com",
+  databaseURL: "https://post-of-the-coast-dev.firebaseio.com",
+  projectId: "post-of-the-coast-dev",
+  storageBucket: "post-of-the-coast-dev.appspot.com",
+  messagingSenderId: "921534180906",
+  appId: "1:921534180906:web:740a8c0d02ad8bc7381d53"
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 firebase.performance();
-firebase.database();
+firebase.firestore();
+let db = firebase.firestore();
+// firebase.database();
+
 var stripe = Stripe('pk_test_WVstOcw3jGMpmSMRSB5x4SKq00yTnhaT5r');
 var elements = stripe.elements();
+var usersRef = db.collection("user");
 
-document.getElementById("register").addEventListener('submit', createAccount)
+var user = firebase.auth().currentUser;
+var  email, cart, cartTotal, uid;
+
+if (user != null) {
+  email = user.email;
+  uid = user.uid;
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+     var userdoc = db.collection("user").doc(user.uid);
+
+     userdoc.get().then(function(doc) {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+    // ...
+  } else {
+     window.location.replace(login.html)
+  }
+});
+
+document.getElementById("register").addEventListener('submit', createAccount);
+
+document.getElementById("login-form").addEventListener("submit", signin);
+
 
 function createAccount(e) {
   e.preventDefault();
@@ -48,6 +84,58 @@ function createAccount(e) {
   
 }
 
+function signin(e){
+ e.preventDefault();
+
+  let email = getInputVal(login-user-name);
+  let password=getinputVal(login-user-password);
+
+ firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+  if (errorMessage != null) {
+    alert(errorMessage);
+  }else{
+    alert("welcome")
+  }
+  
+  // ...
+});
+  firebase.auth().onAuthStateChanged(function(user){
+    console.log(user);
+  });
+}
+
+function signOut(e){
+  
+
+}
+
+function getData(){
+  
+
+  if(uid === null){
+    console.log("Empty Cart");
+  }else{
+    console.log("sign-in");
+//     var docRef = db.collection("cities").doc("SF");
+
+// docRef.get().then(function(doc) {
+//     if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch(function(error) {
+//     console.log("Error getting document:", error);
+// });
+  }
+
+}
 
 
 function getInputVal(id) {
