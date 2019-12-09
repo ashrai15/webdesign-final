9575a3a5b3791d5644ee17c3584609b80a30f563
Noah F <Franck.noahk@gmail.com>
2019-12-08 22:39:39 -0600

    Added addUserToFireStore function which preloads data into firebase.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.addUserToFireStore = functions.auth.user().onCreate((user) => {
+  db.doc('user/'+user.uid).set({
+    email: user.email,
+    cart: [],
+    total_price_cart: 0.0
+  });
+});
